d72aed82b8230465c0c0a6f300468a0dd05d5351
Yurii Pasternak <10pka.muscatel@gmail.com>
2023-11-30 17:15:54 +0200

    Added dapper simple example.

--- SimpleRealEstateWebAPI/Application/Features/RealtyFeatures/GetRealties/GetRealtiesHandler.cs
+++ SimpleRealEstateWebAPI/Application/Features/RealtyFeatures/GetRealties/GetRealtiesHandler.cs
@@ -21,29 +21,32 @@ namespace Application.Features.RealtyFeatures.GetRealties
             _mapper = mapper;
             _connectionService = connectionService;
         }
-       // List<GetRealtiesResponse>
+
         public async Task<PaginatedList<GetRealtiesResponse>> Handle(GetRealtiesRequest request, CancellationToken cancellationToken)
         {
-            //var realtiesQueryable = _context.Realties.AsQueryable();
+            var realtiesQueryable = _context.Realties.AsQueryable();
 
-            //var realtiesPaginated = await realtiesQueryable
-            //      .ProjectTo<GetRealtiesResponse>(_mapper.ConfigurationProvider)
-            //      .PaginatedListAsync(request.PageNumber, request.PageSize);
+            var realtiesPaginated = await realtiesQueryable
+                  .ProjectTo<GetRealtiesResponse>(_mapper.ConfigurationProvider)
+                  .PaginatedListAsync(request.PageNumber, request.PageSize);
 
-            await using SqlConnection sqlConnection = _connectionService.CreateConnection();
+            return realtiesPaginated;
 
-            var sql = $@"SELECT * FROM Realties AS rt
-                      INNER JOIN RealtyPlanningTypes AS RealtyPlanningTypes
-                      ON rt.ID = RealtyPlanningTypes.RealtyId
-                      ORDER BY (SELECT NULL)
-                      OFFSET {(request.PageNumber - 1) * request.PageSize}
-                      ROWS FETCH NEXT {request.PageSize} ROWS ONLY";
 
-            var realties = await sqlConnection.QueryAsync<GetRealtiesResponse>(sql);
 
-            return PaginatedList<GetRealtiesResponse>.Create(realties.ToList(), request.PageNumber, request.PageSize);
 
-            //return realtiesPaginated;
+            //await using SqlConnection sqlConnection = _connectionService.CreateConnection();
+
+            //var sql = $@"SELECT * FROM Realties AS rt
+            //          INNER JOIN RealtyPlanningTypes AS RealtyPlanningTypes
+            //          ON rt.ID = RealtyPlanningTypes.RealtyId
+            //          ORDER BY (SELECT NULL)
+            //          OFFSET {(request.PageNumber - 1) * request.PageSize}
+            //          ROWS FETCH NEXT {request.PageSize} ROWS ONLY";
+
+            //var realties = await sqlConnection.QueryAsync<GetRealtiesResponse>(sql);
+
+            //return PaginatedList<GetRealtiesResponse>.Create(realties.ToList(), request.PageNumber, request.PageSize);
         }
     }
 }
